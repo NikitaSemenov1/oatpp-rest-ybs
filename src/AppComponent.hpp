57d50c93f964379b161c0dde494279b1338b2831
nikita <nikitasemenov0388@gmail.com>
2022-09-13 13:09:33 +0300

    [feat] add PostgreSQL database client

--- src/AppComponent.hpp
+++ src/AppComponent.hpp
@@ -9,12 +9,19 @@
 
 #include "oatpp/core/macro/component.hpp"
 
+#include "oatpp/core/base/CommandLineArguments.hpp"
+
+#include "dto/ConfigDto.hpp"
+
 /**
  *  Class which creates and holds Application components and registers components in oatpp::base::Environment
  *  Order of components initialization is from top to bottom
  */
 class AppComponent {
+private:
+  oatpp::base::CommandLineArguments m_cmdArgs;
 public:
+  AppComponent(const oatpp::base::CommandLineArguments& cmd_Args) : m_cmdArgs(cmd_Args){}
   
   /**
    *  Create ConnectionProvider component which listens on the port
@@ -45,6 +52,35 @@ public:
     return oatpp::parser::json::mapping::ObjectMapper::createShared();
   }());
 
+  OATPP_CREATE_COMPONENT(oatpp::Object<ConfigDto>, config)([this] {
+
+    const char* configPath = CONFIG_PATH;
+    auto objectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
+
+    oatpp::String configText = oatpp::String::loadFromFile(configPath);
+    if (configText) {
+
+      auto profiles = objectMapper->readFromString<oatpp::Fields<oatpp::Object<ConfigDto>>>(configText);
+
+      const char *profileArg = std::getenv("CONFIG_PROFILE"); // first read from env variable
+      if (profileArg == nullptr) {
+        profileArg = m_cmdArgs.getNamedArgumentValue("--profile", "dev"); // if no env variable get from command line
+      }
+
+      OATPP_LOGD("Server", "Loading configuration profile '%s'", profileArg);
+
+      auto profile = profiles.getValueByKey(profileArg, nullptr);
+      if(!profile) {
+        throw std::runtime_error("No configuration profile found. Server won't run.");
+      }
+      return profile;
+
+    }
+
+    OATPP_LOGE("AppComponent", "Can't load configuration file at path '%s'", configPath);
+    throw std::runtime_error("[AppComponent]: Can't load configuration file");
+
+  }());
 };
 
 #endif /* AppComponent_hpp */
